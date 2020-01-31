108debe4fe4f5567ef3242a43210ad7415d0fa68
Gustavo Vilas Bôas <gustavo.boas@startaideia.com.br>
2020-01-31 20:45:02 -0300

    first commit

---
+++ __tests__/sum.spec.js
@@ -0,0 +1,5 @@
+import cortexjs from './../src';
+
+test('test sum funciton', () => {
+  expect(cortexjs()).toBe('hello from cortex');
+});
---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import yogajs from '@yogajs/core';
+
+function App() {
+  return (
+    <div>
+      <h1>{yogajs()}</h1>
+    </div>
+  );
+}
+
+export default App;
